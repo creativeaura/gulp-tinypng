f06e75a7444f257f011eca67231689f546297c5e
Gaurav Jassal <gauravjassal@gmail.com>
2014-02-04 18:26:18 +0000

    first commit for this plugin

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var tingpng = require('./index');
+
+
+const TINYPNG_API = "8FiQFj9oWwEyTBHMMwxjvuYNx05Fphk2";
+
+gulp.task('tinypng', function(){
+  var stream;
+  stream = gulp.src('original_images/**/*.png')
+            .pipe(tingpng(TINYPNG_API))
+            .pipe(gulp.dest('compressed_images'));
+  return stream;
+});
---
+++ test.js
@@ -0,0 +1,10 @@
+'use strict';
+var fs = require('fs');
+var assert = require('assert');
+var gutil = require('gulp-util');
+var tinypng = require('./index');
+
+it('should only compress png files', function (cb) {
+  assert(true);
+  cb();
+});
