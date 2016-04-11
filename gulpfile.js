9c5a7228fab10625de4b8706f944a75f93801f49
Jesse Zhu <jesse@itjesse.cn>
2016-04-11 16:17:47 +0800

    add cache

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
-var tingpng = require('./index');
+var tinypng = require('./index');
 
 
 const TINYPNG_API = "8FiQFj9oWwEyTBHMMwxjvuYNx05Fphk2";
@@ -9,7 +9,10 @@ gulp.task('tinypng', function(){
   var stream;
 
   stream = gulp.src('original_images/**/*.png')
-            .pipe(tingpng(TINYPNG_API))
+            .pipe(tinypng({
+              apiKey: TINYPNG_API,
+              cached: true
+            }))
             .pipe(gulp.dest('compressed_images'));
   return stream;
 });
